a9a8dad77bb8f0b80e6daaf1f351ffac72774c31
joao034 <84355621+joao034@users.noreply.github.com>
2021-07-15 19:08:15 -0500

    Estructura del proyecto

---
+++ servicios/conexion.php
@@ -0,0 +1,21 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="bodegas";
+$conn= mysqli_connect($servername,$username,$password,$dbname);
+$mysqli = new mysqli($servername,$username,$password,$dbname);
+if(!$mysqli)
+{
+    die("Error en la conexion".mysqli_connect_error());
+}
+function utf8_converter($array)
+{
+    array_walk_recursive($array, function($item) {
+        $item =utf8_encode($item);
+    });
+    return json_encode($array);
+}
+
+
+?>
---
+++ servicios/login.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_REQUEST['usuario']) && isset($_REQUEST['password'])){
+    include 'conexion.php';
+
+    $usuario = $_REQUEST['usuario'];
+    $password = $_REQUEST['password'];
+
+    $query = "SELECT * FROM USUARIOS WHERE user = '$usuario' AND pass_usu = '$password'";
+    $resultado = mysqli_query($conn, $query);
+    $arreglo = array();
+    while($fila = mysqli_fetch_assoc($resultado)){
+        array_push($arreglo, $fila);
+    }
+    echo json_encode($arreglo, JSON_FORCE_OBJECT);
+    mysqli_close($conn);
+}
+?>
