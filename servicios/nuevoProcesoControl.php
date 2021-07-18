1123082175bdc7708215ac4da2ee294f46a2e9bf
joao034 <84355621+joao034@users.noreply.github.com>
2021-07-18 14:47:31 -0500

    Servicio nuevo proceso de validacion

---
+++ servicios/nuevoProcesoControl.php
@@ -0,0 +1,26 @@
+<?php
+include 'conexion.php';
+$titulo = $_REQUEST['titulo'];
+$fecha = $_REQUEST['fecha'];
+$funcionarios = $_REQUEST['funcionarios'];
+$queryProceso = "SELECT (COUNT(*)) FROM procesosheader";
+$resultado = mysqli_query($conn, $queryProceso);
+if (mysqli_num_rows($resultado) > 0) {
+	while($rowData = mysqli_fetch_array($resultado)){
+  		$id = $rowData["0"];
+	}
+}
+
+if(isset($titulo) && isset($funcionarios) && isset($fecha)){
+    $query1 = "INSERT INTO procesosheader (ID_PRO, TIT_PRO, FEC_PRO)  VALUES (($id + 1), '$titulo', '$fecha')";
+    $resultado1 = mysqli_query($conn, $query1);
+    for($i = 0; $i < sizeof($funcionarios) ;$i++){
+        $query2 = "INSERT INTO procesosdetail (ID_FUN_PER, ID_PRO_PER, OBS_REV)  VALUES ('$funcionarios[$i]',($id + 1),'REVISAR')";
+        $resultado2 = mysqli_query($conn, $query2);
+    } 
+        echo json_encode("El proceso se creo correctamente");
+}else{
+    echo json_encode('Asignar los valores correspondientes');
+}
+
+?>
