ee1b411db3973dc8e1d747eb12accbc487fa2e9a
Mateo <jmartinez6208@uta.edu.ec>
2021-07-27 16:07:19 -0500

    Servicio actualizar procesos

---
+++ servicios/cargarIdProcesos.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+include 'conexion.php';
+
+$titulo = $_REQUEST['titulo'];
+$sqlSelect = "SELECT * FROM procesosheader where TIT_PRO = '$titulo'";
+//Ejecuta el query
+$respuesta = $conn->query($sqlSelect);
+$result = array();
+if ($respuesta->num_rows > 0){
+    //Recorre cada fila
+    while ($filaFuncionario = $respuesta->fetch_assoc()){
+        //Guarda cada fila en una posicion del array
+        array_push($result, $filaFuncionario);
+    }
+}else{
+    $result = "No hay procesos";
+}
+echo json_encode($result, JSON_FORCE_OBJECT);
+?>
