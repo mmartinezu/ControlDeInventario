46e55c7f4ee33a701ddd519a4a94366270cf3563
Mateo <jmartinez6208@uta.edu.ec>
2021-08-11 17:08:13 -0500

    Servicios guardado proceso

--- servicios/actualizarEstadoProceso.php
+++ servicios/actualizarEstadoProceso.php
@@ -1,12 +1,10 @@
 <?php
 include 'conexion.php';
 $observacion = $_REQUEST['obs'];
-$estadoActivo = $_REQUEST['estActivo'];
-$activosFun = $_REQUEST['activosFun'];
 $estadoProceso = $_REQUEST['estadoProceso'];
 $idProcesoDet = $_REQUEST['idProcesoDet'];
 
-if(isset($estadoProceso) && isset($estadoActivo) && isset($activosFun) && isset($idProcesoDet) ){
+if(isset($idProcesoDet) ){
 
     $updateDetail = "update procesosdetail
                      set EST_PRO = '$estadoProceso',
@@ -14,12 +12,6 @@ if(isset($estadoProceso) && isset($estadoActivo) && isset($activosFun) && isset(
                      where ID_PRO_DET = $idProcesoDet";
     $result = mysqli_query($conn, $updateDetail);
 
-    for($i = 0; $i < sizeof($activosFun) ; $i++){
-        $updateActivo = "update activos
-                         set REV_ACT = '$estadoActivo[$i]'
-                         where ID_ACT = '$activosFun[$i]'";
-        $result1 = mysqli_query($conn, $updateActivo);
-    }
     echo json_encode("Actualizado correctamente");
 }else{
     echo json_encode("Asignar valores");
